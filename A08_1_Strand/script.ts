f006f13c6290ea345a77181ac70a87f6ac78aba0
AnjaWeber <92021165+AnjaWeber@users.noreply.github.com>
2022-05-13 21:05:24 +0200

    scale und transform ergänzt

--- A08_1_Strand/script.ts
+++ A08_1_Strand/script.ts
@@ -19,29 +19,38 @@ namespace BeachSceneAnja {
         drawCloud({ x: 200, y: 145 }, { x: 120, y: 65 });
         drawBush({ x: 60, y: 500 }, { x: 120, y: 100 });
         drawBush({ x: 720, y: 600 }, { x: 180, y: 80 });
-        drawBoat({ x: 150, y: 160 }, "#f3c4cf");
-        drawBoat({ x: 600, y: 110 }, "#CCBBEE");
+        drawBoat({ x: 150, y: 160 }, {x: 1, y: 1}, "#f3c4cf");
+        drawBoat({ x: 600, y: 150 }, {x: 0.5, y: 0.5}, "#CCBBEE");
+        drawTowel2({ x: 300, y: 400 });
         drawPerson({ x: 300, y: 410 }, 'shirt', "#8800FF");
-        drawPerson({ x: 400, y: 410 }, 'skirt', '#C4FAF8');
-        drawSwimmer({ x: 300, y: 310 });
-        drawSwimmer({ x: 400, y: 290 });
-        drawSurfBoard({ x: 200, y: 500 });
+        drawPerson({ x: 410, y: 420 }, 'skirt', '#C4FAF8');
+        drawSwimmer({ x: 300, y: 220 }, {x: 0.5, y: 0.5},);
+        drawSwimmer({ x: 400, y: 290 } , {x: 1, y: 1});
+        drawSurfBoard({ x: 540, y: 290 });
         drawSurfer({ x: 500, y: 200 });
-       
+        drawTowel1({ x: 500, y: 400 });
+        drawTowel3({ x: 200, y: 400 });
+        drawMoewe({ x: 100, y: 400 });
+
+
+
+
 
 
     }
 
     function drawSky(_position: Vector): void {
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
         crc2.beginPath();
-        crc2.moveTo(_position.x, _position.y);
-        crc2.lineTo(_position.x, _position.y + 200);
-        crc2.lineTo(_position.x + 800, _position.y + 200);
-        crc2.lineTo(_position.x + 800, _position.y);
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, 200);
+        crc2.lineTo(800, 200);
+        crc2.lineTo(800, 0);
         crc2.closePath();
-        
 
-        let gradient: CanvasGradient = crc2.createLinearGradient(_position.x + 0, _position.y + 0, _position.x + 0, _position.y + 200);
+
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0, 0, 200);
 
         gradient.addColorStop(0, "#0047ab");
         gradient.addColorStop(.5, "#0088FF");
@@ -49,17 +58,20 @@ namespace BeachSceneAnja {
 
         crc2.fillStyle = gradient;
         crc2.fill();
+        crc2.restore();
     }
 
     function drawSea(_position: Vector): void {
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
         crc2.beginPath();
-        crc2.moveTo(_position.x, _position.y);
-        crc2.lineTo(_position.x, _position.y + 300);
-        crc2.lineTo(_position.x + 800, _position.y + 300);
-        crc2.lineTo(_position.x + 800, _position.y);
+        crc2.moveTo(0, 0);
+        crc2.lineTo(0, 300);
+        crc2.lineTo(800, 300);
+        crc2.lineTo(800, 0);
         crc2.closePath();
-        
-        let gradient: CanvasGradient = crc2.createLinearGradient(_position.x + 0, _position.y + 0, _position.x + 0, _position.y + 300);
+
+        let gradient: CanvasGradient = crc2.createLinearGradient(0, 0,  0, 300);
 
         gradient.addColorStop(0, "#7bb5d8");
         gradient.addColorStop(.5, "#3976a3");
@@ -67,33 +79,40 @@ namespace BeachSceneAnja {
 
         crc2.fillStyle = gradient;
         crc2.fill();
+        crc2.restore();
     }
 
     function drawBeach(_position: Vector): void {
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
         crc2.beginPath();
-        crc2.moveTo(_position.x, _position.y);
-        crc2.bezierCurveTo(_position.x + 200, _position.y + 100, _position.x + 600, _position.y + 130, _position.x + 800, _position.y + 40);
-        crc2.lineTo(_position.x + 800, _position.y + 300);
-        crc2.lineTo(_position.x, _position.y + 300);
+        crc2.moveTo(0, 0);
+        crc2.bezierCurveTo(200, 100, 600, 130, 800, 40);
+        crc2.lineTo(800, 300);
+        crc2.lineTo(0, 300);
         crc2.closePath();
         crc2.fillStyle = "#fbdea4";
         crc2.fill();
+        crc2.restore();
     }
 
     function drawSun(_position: Vector): void {
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
         crc2.beginPath();
-        crc2.arc(_position.x, _position.y, 150, 0, 2 * Math.PI);
-        let gradient: CanvasGradient = crc2.createRadialGradient(_position.x, _position.y, 75, _position.x, _position.y, 150);
+        crc2.arc(0, 0, 150, 0, 2 * Math.PI);
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 75, 0, 0, 150);
         gradient.addColorStop(0, "rgba(255,166,70, 0.75)");
-        
+
         gradient.addColorStop(1, "rgba(255,166,70, 0)");
         crc2.fillStyle = gradient;
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x, _position.y, 75, 0, 2 * Math.PI);
+        crc2.arc(0, 0, 75, 0, 2 * Math.PI);
         crc2.fillStyle = "#ffa646";
         crc2.fill();
+        crc2.restore();
     }
 
     function drawCloud(_position: Vector, _size: Vector): void {
@@ -146,19 +165,22 @@ namespace BeachSceneAnja {
         crc2.restore();
     }
 
-    function drawBoat(_position: Vector, _boatColor: string): void {
+    function drawBoat(_position: Vector, _scale: Vector, _boatColor: string): void {
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.scale(_scale.x, _scale.y)
         crc2.beginPath();
-        crc2.moveTo(_position.x, _position.y + 75);
-        crc2.bezierCurveTo(_position.x + 25, _position.y + 130, _position.x + 75, _position.y + 130, _position.x + 100, _position.y + 75);
+        crc2.moveTo(0, 75);
+        crc2.bezierCurveTo(25, 130, 75, 130, 100, 75);
         crc2.closePath();
         crc2.stroke();
         crc2.fillStyle = _boatColor;
         crc2.fill();
 
         crc2.beginPath();
-        crc2.moveTo(_position.x + 50, _position.y + 25);
-        crc2.lineTo(_position.x + 75, _position.y + 50);
-        crc2.lineTo(_position.x + 50, _position.y + 50);
+        crc2.moveTo(50, 25);
+        crc2.lineTo(75, 50);
+        crc2.lineTo(50, 50);
         crc2.closePath();
         crc2.stroke();
         crc2.fillStyle = "#fffff0";
@@ -166,58 +188,58 @@ namespace BeachSceneAnja {
 
 
         crc2.beginPath();
-        crc2.moveTo(_position.x + 50, _position.y + 75);
-        crc2.lineTo(_position.x + 50, _position.y + 25);
+        crc2.moveTo(50, 75);
+        crc2.lineTo(50, 25);
         crc2.strokeStyle = "#5b3a29";
         crc2.stroke();
-
-
-
+        crc2.restore();
 
     }
 
     function drawPerson(_position: Vector, _clothes: 'skirt' | 'shirt', _color: string): void {
         // Hände
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
         crc2.beginPath();
-        crc2.arc(_position.x + 12.5, _position.y + 42, 5, 0, 2 * Math.PI);
+        crc2.arc(12.5, 42, 5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x + 62.5, _position.y + 42, 5, 0, 2 * Math.PI);
+        crc2.arc(62.5, 42, 5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
         // Füße
         crc2.beginPath();
-        crc2.arc(_position.x + 30, _position.y + 88, 5.5, 0, 2 * Math.PI);
+        crc2.arc(30, 88, 5.5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x + 45, _position.y + 88, 5.5, 0, 2 * Math.PI);
+        crc2.arc(45, 88, 5.5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         //shirt/skirt
         crc2.beginPath();
-        crc2.moveTo(_position.x + 37.5, _position.y + 20);
-        crc2.lineTo(_position.x + 12.5, _position.y + 37.5);
-        crc2.lineTo(_position.x + 15, _position.y + 45);
-        crc2.lineTo(_position.x + 25, _position.y + 42);
+        crc2.moveTo(37.5, 20);
+        crc2.lineTo(12.5, 37.5);
+        crc2.lineTo(15, 45);
+        crc2.lineTo(25, 42);
         if (_clothes === 'shirt') {
-            crc2.lineTo(_position.x + 22, _position.y + 57);
-            crc2.lineTo(_position.x + 53, _position.y + 57);
+            crc2.lineTo(22, 57);
+            crc2.lineTo(53, 57);
         } else {
-            crc2.lineTo(_position.x + 20, _position.y + 87.5);
-            crc2.lineTo(_position.x + 55, _position.y + 87.5);
+            crc2.lineTo(20, 87.5);
+            crc2.lineTo(55, 87.5);
         }
-        crc2.lineTo(_position.x + 50, _position.y + 42);
-        crc2.lineTo(_position.x + 60, _position.y + 45);
-        crc2.lineTo(_position.x + 62.5, _position.y + 37.5);
+        crc2.lineTo(50, 42);
+        crc2.lineTo(60, 45);
+        crc2.lineTo(62.5, 37.5);
         crc2.closePath();
         crc2.stroke();
         crc2.fillStyle = _color;
@@ -226,14 +248,14 @@ namespace BeachSceneAnja {
         if (_clothes === 'shirt') {
             // hose
             crc2.beginPath();
-            crc2.moveTo(_position.x + 25, _position.y + 57);
-            crc2.lineTo(_position.x + 27, _position.y + 87.5);
-            crc2.lineTo(_position.x + 35, _position.y + 87.5);
-            crc2.lineTo(_position.x + 36, _position.y + 63);
-            crc2.lineTo(_position.x + 39, _position.y + 63);
-            crc2.lineTo(_position.x + 40, _position.y + 87.5);
-            crc2.lineTo(_position.x + 48, _position.y + 87.5);
-            crc2.lineTo(_position.x + 50, _position.y + 57);
+            crc2.moveTo(25, 57);
+            crc2.lineTo(27, 87.5);
+            crc2.lineTo(35, 87.5);
+            crc2.lineTo(36, 63);
+            crc2.lineTo(39, 63);
+            crc2.lineTo(40, 87.5);
+            crc2.lineTo(48, 87.5);
+            crc2.lineTo(50, 57);
             crc2.closePath();
             crc2.stroke();
             crc2.fillStyle = "#343F51";
@@ -243,91 +265,102 @@ namespace BeachSceneAnja {
 
         // Kopf
         crc2.beginPath();
-        crc2.arc(_position.x + 37.5, _position.y + 12.5, 12.5, 0, 2 * Math.PI);
+        crc2.arc(37.5, 12.5, 12.5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
+        crc2.restore();
     }
 
-    function drawSwimmer(_position: Vector): void {
+    function drawSwimmer(_position: Vector, _scale: Vector): void {
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.scale(_scale.x, _scale.y);
+
         crc2.beginPath();
-        crc2.moveTo(_position.x + 37.5, _position.y + 20);
-        crc2.lineTo(_position.x + 12.5, _position.y + 37.5);
-        crc2.lineTo(_position.x + 15, _position.y + 45);
-        crc2.lineTo(_position.x + 25, _position.y + 42);
+        crc2.moveTo(37.5, 20);
+        crc2.lineTo(12.5, 37.5);
+        crc2.lineTo(15, 45);
+        crc2.lineTo(25, 42);
 
-        crc2.lineTo(_position.x + 25, _position.y + 57);
-        crc2.lineTo(_position.x + 50, _position.y + 57);
+        crc2.lineTo(25, 57);
+        crc2.lineTo(50, 57);
 
 
 
-        crc2.lineTo(_position.x + 50, _position.y + 42);
-        crc2.lineTo(_position.x + 60, _position.y + 45);
-        crc2.lineTo(_position.x + 62.5, _position.y + 37.5);
+        crc2.lineTo(50, 42);
+        crc2.lineTo(60, 45);
+        crc2.lineTo(62.5, 37.5);
         crc2.closePath();
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x + 12.5, _position.y + 42, 5, 0, 2 * Math.PI);
+        crc2.arc(12.5, 42, 5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x + 62.5, _position.y + 42, 5, 0, 2 * Math.PI);
+        crc2.arc(62.5, 42, 5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x + 37.5, _position.y + 12.5, 12.5, 0, 2 * Math.PI);
+        crc2.arc(37.5, 12.5, 12.5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
+
+        crc2.restore();
     }
 
     function drawSurfer(_position: Vector): void {
         //Kopf
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
         crc2.beginPath();
-        crc2.arc(_position.x + 37.5, _position.y + 12.5, 12.5, 0, 2 * Math.PI);
+        crc2.arc(37.5, 12.5, 12.5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         //Körper
+
+        //linker Arm
         crc2.beginPath();
-        crc2.moveTo(_position.x + 37.5, _position.y + 20);
-        crc2.lineTo(_position.x + 12.5, _position.y + 37.5);
-        crc2.lineTo(_position.x + 15, _position.y + 45);
-        crc2.lineTo(_position.x + 25, _position.y + 42);
+        crc2.moveTo(37.5, 20);
+        crc2.lineTo(12.5, 37.5);
+        crc2.lineTo(15, 45);
+        crc2.lineTo(25, 42);
 
-        crc2.lineTo(_position.x + 25, _position.y + 57);
-        crc2.lineTo(_position.x + 50, _position.y + 57);
+        crc2.lineTo(25, 57);
+        crc2.lineTo(50, 57);
 
-        crc2.lineTo(_position.x + 50, _position.y + 42);
-        crc2.lineTo(_position.x + 60, _position.y + 45);
-        crc2.lineTo(_position.x + 62.5, _position.y + 37.5);
+        crc2.lineTo(50, 42);
+        crc2.lineTo(60, 45);
+        crc2.lineTo(62.5, 37.5);
         crc2.closePath();
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x + 12.5, _position.y + 42, 5, 0, 2 * Math.PI);
+        crc2.arc(12.5, 42, 5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x + 62.5, _position.y + 42, 5, 0, 2 * Math.PI);
+        crc2.arc(62.5, 42, 5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x + 37.5, _position.y + 12.5, 12.5, 0, 2 * Math.PI);
+        crc2.arc(37.5, 12.5, 12.5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
@@ -335,14 +368,14 @@ namespace BeachSceneAnja {
         //Badehose
 
         crc2.beginPath();
-        crc2.moveTo(_position.x + 25, _position.y + 57);
-        crc2.lineTo(_position.x + 27, _position.y + 75);
-        crc2.lineTo(_position.x + 35, _position.y + 75);
-        crc2.lineTo(_position.x + 36, _position.y + 63);
-        crc2.lineTo(_position.x + 39, _position.y + 63);
-        crc2.lineTo(_position.x + 40, _position.y + 75);
-        crc2.lineTo(_position.x + 48, _position.y + 75);
-        crc2.lineTo(_position.x + 50, _position.y + 57);
+        crc2.moveTo(25, 57);
+        crc2.lineTo(27, 75);
+        crc2.lineTo(35, 75);
+        crc2.lineTo(36, 63);
+        crc2.lineTo(39, 63);
+        crc2.lineTo(40, 75);
+        crc2.lineTo(48, 75);
+        crc2.lineTo(50, 57);
         crc2.closePath();
         crc2.stroke();
         crc2.fillStyle = "#E71837";
@@ -350,10 +383,10 @@ namespace BeachSceneAnja {
 
         //Bein links
         crc2.beginPath();
-        crc2.moveTo(_position.x + 28, _position.y + 75);
-        crc2.lineTo(_position.x + 28, _position.y + 90);
-        crc2.lineTo(_position.x + 34, _position.y + 90);
-        crc2.lineTo(_position.x + 34, _position.y + 75);
+        crc2.moveTo(28, 75);
+        crc2.lineTo(28, 90);
+        crc2.lineTo(34, 90);
+        crc2.lineTo(34, 75);
         crc2.closePath();
 
         crc2.stroke();
@@ -363,10 +396,10 @@ namespace BeachSceneAnja {
 
         //Bein rechts
         crc2.beginPath();
-        crc2.moveTo(_position.x + 46.5, _position.y + 75);
-        crc2.lineTo(_position.x + 46.5, _position.y + 90);
-        crc2.lineTo(_position.x + 40, _position.y + 90);
-        crc2.lineTo(_position.x + 40, _position.y + 75);
+        crc2.moveTo(46.5, 75);
+        crc2.lineTo(46.5, 90);
+        crc2.lineTo(40, 90);
+        crc2.lineTo(40, 75);
         crc2.closePath();
 
         crc2.stroke();
@@ -375,25 +408,100 @@ namespace BeachSceneAnja {
 
         //Füße
         crc2.beginPath();
-        crc2.arc(_position.x + 30, _position.y + 88, 5, 0, 2 * Math.PI);
+        crc2.arc(30, 88, 5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
 
         crc2.beginPath();
-        crc2.arc(_position.x + 45, _position.y + 88, 5, 0, 2 * Math.PI);
+        crc2.arc(45, 88, 5, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FEC7B2";
         crc2.fill();
+        crc2.restore();
     }
 
     //Surfboard
     function drawSurfBoard(_position: Vector): void {
 
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
         crc2.beginPath();
-        crc2.ellipse(540, 290, 25, 60, Math.PI / 3, 0, 2 * Math.PI);
+        crc2.ellipse(0, 0, 25, 60, Math.PI / 3, 0, 2 * Math.PI);
         crc2.stroke();
         crc2.fillStyle = "#FFFF99";
+        crc2.fill();
+        crc2.restore();
+    }
+
+    //Handtuch
+
+    function drawTowel1(_position: Vector): void {
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.moveTo(0, 0);
+        crc2.lineTo(50, 0);
+        crc2.lineTo(125, 75);
+        crc2.lineTo(50, 75);
+        crc2.closePath();
+        crc2.stroke();
+        crc2.fillStyle = "#FEA95E";
+        crc2.fill();
+        crc2.restore();
+
+
+    }
+    function drawTowel2(_position: Vector): void {
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.moveTo(75, 0);
+        crc2.lineTo(125, 0);
+        crc2.lineTo(125, 75);
+        crc2.lineTo(50, 75);
+        crc2.closePath();
+        crc2.stroke();
+        crc2.fillStyle = "#CFF0CC";
+        crc2.fill();
+        crc2.restore();
+    }
+    function drawTowel3(_position: Vector): void {
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.beginPath();
+        crc2.moveTo(75, 0);
+        crc2.lineTo(125, 0);
+        crc2.lineTo(75, 75);
+        crc2.lineTo(0, 75);
+        crc2.closePath();
+        crc2.stroke();
+        crc2.fillStyle = "#FBCCD1";
+        crc2.fill();
+        crc2.restore();
+    }
+
+    function drawMoewe(_position: Vector): void {
+        crc2.beginPath();
+        crc2.moveTo(650, 100);
+        crc2.quadraticCurveTo(685, 75, 700, 100);
+        crc2.moveTo(700, 100);
+        crc2.quadraticCurveTo(720, 75, 750, 100);
+        crc2.moveTo(750, 100);
+        crc2.quadraticCurveTo(720, 86, 700, 100);
+        crc2.moveTo(700, 100);
+        crc2.quadraticCurveTo(685, 86, 650, 100);
+
+        crc2.strokeStyle = "#000000";
+        crc2.fillStyle = "#FFFFFF";
+        crc2.stroke();
+        crc2.fill();
+
+        crc2.beginPath();
+        // crc2.moveTo(_position.x, _position.y);
+        crc2.arc(_position.x + 600, _position.y - 300, 8, 0, 2 * Math.PI);
+        crc2.stroke();
+        crc2.fillStyle = "#FFFFFF";
         crc2.fill();
     }
 }
